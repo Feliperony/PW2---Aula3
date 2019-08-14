b0743c83dc414a83b2d2c1391a4d2f173001c3b8
Feliperony <rony.alvessobral@hotmail.com>
2019-08-14 19:48:12 -0300

    Revisão - TS

---
+++ impl/revisao.js
@@ -0,0 +1,5 @@
+var nota1 = 5;
+var nota2 = 5;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A média é: " + media);
---
+++ revisao.ts
@@ -0,0 +1,6 @@
+let nota1 : number = 5
+let nota2 : number = 5
+let nota3 : number = 5
+
+let media = (nota1 + nota2 + nota3) /3
+console.log("A média é: " + media)
