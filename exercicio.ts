515bbd8ad404bf5ff47e64fc7bd9e96293a3c769
Feliperony <rony.alvessobral@hotmail.com>
2019-08-14 22:25:19 -0300

    Volume da caixa

---
+++ exercicio.ts
@@ -0,0 +1,7 @@
+let comprimento : number = 18
+let largura : number = 6
+let altura : number = 5
+
+let volume = (comprimento:number, largura:number, altura:number)=> comprimento * largura + altura
+
+console.log(`O volume da Caixa é ${volume(comprimento,largura,altura)}`)
---
+++ impl/exercicio.js
@@ -0,0 +1,5 @@
+var comprimento = 18;
+var largura = 6;
+var altura = 5;
+var volume = function (comprimento, largura, altura) { return comprimento * largura + altura; };
+console.log("O volume da Caixa \u00E9 " + volume(comprimento, largura, altura));
