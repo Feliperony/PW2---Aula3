3ed9e4b55bafee27f882b6175ca18a00b49bf1ec
Feliperony <rony.alvessobral@hotmail.com>
2019-08-14 22:12:58 -0300

    Valor padrão (funcão)

--- impl/revisao.js
+++ impl/revisao.js
@@ -1,8 +1,9 @@
 var n1 = 10;
 var n2 = 5;
-var n3 = 7;
+//let n3 : number = 7
 //let media = function (nota1 + nota2 + nota3) /3
 var fMedia = function (nota1, nota2, nota3) {
+    if (nota3 === void 0) { nota3 = 7; }
     return (nota1 + nota2 + nota3) / 3;
 };
-console.log("A media \u00E9 : " + fMedia(n1, n2, n3));
+console.log("A media \u00E9 : " + fMedia(n1, n2));
--- revisao.ts
+++ revisao.ts
@@ -1,10 +1,9 @@
 let n1 : number = 10
 let n2 : number = 5
-let n3 : number = 7
+//let n3 : number = 7
 
 //let media = function (nota1 + nota2 + nota3) /3
 
-let fMedia = function (nota1:number, nota2:number, nota3:number):number{
-    return(nota1 + nota2 + nota3) /3
-}
-console.log(`A media é : ${fMedia(n1,n2,n3)}`)
+let fMedia = (nota1:number, nota2:number, nota3:number=7)=>(nota1 + nota2 + nota3) /3
+
+console.log(`A media é : ${fMedia(n1,n2)}`)
